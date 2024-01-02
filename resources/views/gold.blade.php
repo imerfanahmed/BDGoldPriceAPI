834849dbf617ccfc78ca231e6779ea910170e3d1
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2024-01-02 22:00:11 +0600

    md

--- resources/views/gold.blade.php
+++ resources/views/gold.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>Title</title>
+        <title>BD GOLD Price Tracker and API</title>
         <!-- Required meta tags -->
         <meta charset="utf-8" />
         <meta
